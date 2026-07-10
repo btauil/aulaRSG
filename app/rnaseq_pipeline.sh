65b68cb93b997ee2101794b8239d4176ec476df6
Instructor <instructor@example.com>
2026-07-10 20:21:13 -0300

    feat: adicionando o salmon

--- app/rnaseq_pipeline.sh
+++ app/rnaseq_pipeline.sh
@@ -5,10 +5,17 @@ set -euo pipefail
 
 # Inputs
 INPUT_FASTQ="${1:-}" # Definindo meu input
+INPUT_INDEX="${2:-}" # input pasta de index salmon
 
 if [ -z "$INPUT_FASTQ" ]; then
     echo "Error: Nenhum input fastq entregue."
     echo "bash rnaseq_pipeline.sh <input_fastq.gz>"
+    exit 1
+fi
+
+if [ -z "$INPUT_INDEX" ]; then
+    echo "Error: Nenhum diretorio de indice entregue."
+    echo "bash rnaseq_pipeline.sh <input_fastq.gz> <input_indice/>"
     exit 1
 fi
 
@@ -32,3 +39,13 @@ echo "[2/3] Rodando o Trimmomatic..."
 trimmomatic SE -phred33 "$INPUT_FASTQ" "$TRIMMED_FASTQ" LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
 # 3. Salmon
+RESULTS_SALMON="resultados/salmon"
+mkdir -p "$RESULTS_SALMON"
+
+echo "[3/3] Rodando o Salmon..."
+
+salmon quant -i $INPUT_INDEX -l A -r "$TRIMMED_FASTQ" -o "$RESULTS_SALMON/quant_output"
+
+echo "================================="
+echo "Pipeline RNAseq concluida!"
+echo "================================="
