b34c31b68cfb839ce8608824b51ecbe1903cb305
Instructor <instructor@example.com>
2026-07-10 17:25:06 -0300

    feat: arquivo de teste pronto, iniciando o arquivo da pipeline e arquivo de download

--- app/fetch_data.sh
+++ app/fetch_data.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # em ./data, o volume montado do docker e persistem depois que o container terminar.
 #
 # rode com:
-# docker run --rm -u 0:0 -v "$(pwd):/work -w /work --entrypoint /usr/local/bin/_entrypoint.sh rnaseq bash fetch_real_data.sh 
+# bash fetch_data.sh 
 
 REF_URL="https://ftp.ensembl.org/pub/release-110/fasta/saccharomyces_cerevisiae/cdna/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa.gz"
 READS_URL="ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR797/SRR797058/SRR797058_1.fastq.gz"
@@ -18,7 +18,7 @@ echo "[1/3] Fazendo o download do transcriptoma de referência de Saccharomyces 
 curl -sL "$REF_URL" -o data/ref/S_cerevisia.cdna.all.fa.gz
 
 echo "[2/3] Montando o index Salmon..."
-salmon index -t data/ref/S_cerevisia.cdna.all.fa.gz -i data/sc_index
+docker run --rm -v "$(pwd):/work" -w /work rnaseq:1.0 salmon index -t data/ref/S_cerevisia.cdna.all.fa.gz -i data/sc_index
 
 echo "[3/3] Download + subamostragem de reads SRR787058..."
 curl -sL "$READS_URL" | zcat | head -n 160000 | gzip > data/SRR797058_subamostra.fastq.gz
